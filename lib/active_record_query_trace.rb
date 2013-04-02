f5d708ec50a094025578ca6e4758c9ea79979185
Brian Buchalter <brianb@endpoint.com>
2013-04-02 01:24:55 -0400

    Allow full backtraces as option

--- lib/active_record_query_trace.rb
+++ lib/active_record_query_trace.rb
@@ -4,6 +4,7 @@ module ActiveRecordQueryTrace
 
   class << self
     attr_accessor :enabled
+    attr_accessor :level
   end
 
   module ActiveRecord
@@ -12,16 +13,21 @@ module ActiveRecordQueryTrace
       def initialize
         super
         ActiveRecordQueryTrace.enabled = false
+        ActiveRecordQueryTrace.level = :app
       end
 
       def sql(event)
         if ActiveRecordQueryTrace.enabled
-          debug("\e[1m\e[35m\e[1m\e[47mCalled from:\e[0m " + clean_trace(caller[2..-2]).join("\n "))
+          debug("\e[1m\e[35m\e[1m\e[47mCalled from:\e[0m " + clean_trace(caller).join("\n "))
         end
       end
 
       def clean_trace(trace)
-        Rails.respond_to?(:backtrace_cleaner) ? Rails.backtrace_cleaner.clean(trace) : trace
+        if ActiveRecordQueryTrace.level == :full
+          trace
+        else
+          Rails.respond_to?(:backtrace_cleaner) ? Rails.backtrace_cleaner.clean(trace) : trace
+        end
       end
 
       attach_to :active_record
