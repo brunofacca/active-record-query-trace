f22853702e08c168437041939922efd27c59f66c
Fabrizio Monti <f.monti@datocms.com>
2023-07-26 11:10:52 +0200

    Fix for RSpec/BeNil

--- spec/active_record_query_trace_spec.rb
+++ spec/active_record_query_trace_spec.rb
@@ -4,6 +4,6 @@ require 'spec_helper'
 
 RSpec.describe ActiveRecordQueryTrace do
   it 'has a version number' do
-    expect(ActiveRecordQueryTrace::VERSION).not_to be nil
+    expect(ActiveRecordQueryTrace::VERSION).not_to be_nil
   end
 end
